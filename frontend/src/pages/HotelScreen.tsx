a43ef4a5fd6df51dbc40c40374ef14f884e32bd6
uuganbayar <d.uuganbayar.stu@gmail.com>
2022-08-26 22:20:07 +0800

    aldaa zaslaa

--- frontend/src/pages/HotelScreen.tsx
+++ frontend/src/pages/HotelScreen.tsx
@@ -1,4 +1,4 @@
-import { Tabs, TabsProps } from 'antd'
+import { Tabs} from 'antd'
 import React from 'react'
 import Footer from '../components/footer/Footer'
 import HotelGallery from '../components/hotelInfos/HotelGallery'
@@ -10,17 +10,17 @@ import Room from '../components/hotelInfos/Room'
 import SearchHeader from '../components/searchHeader'
 import { cityImages } from '../data/data'
 
-import { Sticky, StickyContainer } from 'react-sticky';
+// import { Sticky, StickyContainer } from 'react-sticky';
 
 const { TabPane } = Tabs;
 
-const renderTabBar: TabsProps['renderTabBar'] = (props, DefaultTabBar) => (
-  <Sticky bottomOffset={80}>
-    {({ style }) => (
-      <DefaultTabBar {...props} className="site-custom-tab-bar" style={{ ...style }} />
-    )}
-  </Sticky>
-);
+// const renderTabBar: TabsProps['renderTabBar'] = (props, DefaultTabBar) => (
+//   <Sticky bottomOffset={80}>
+//     {({ style }) => (
+//       <DefaultTabBar {...props} className="site-custom-tab-bar" style={{ ...style }} />
+//     )}
+//   </Sticky>
+// );
 const reverseButton = <button className='btn-primary hidden font-medium md:block'>Өрөө захиалах</button>
 
 
@@ -31,8 +31,8 @@ function HotelScreen() {
       <div className='md:pt-[110px]'>
         <div className='md:container'>
           <HotelGallery images={cityImages(10)} />
-          <StickyContainer>
-            <Tabs tabBarExtraContent={reverseButton} size="middle" className='mb-14 border-t md:border-0 border-gray-light' renderTabBar={renderTabBar}>
+          {/* <StickyContainer> */}
+            <Tabs size="middle" className='mb-14 border-t md:border-0 border-gray-light' tabBarExtraContent={reverseButton}>
               <TabPane tab="Тойм" key="1">
                 <Overview />
               </TabPane>
@@ -52,7 +52,7 @@ function HotelScreen() {
                 <HotelLocation />
               </TabPane> */}
             </Tabs>
-          </StickyContainer>
+          {/* </StickyContainer> */}
         </div>
       </div>
       <Footer />
