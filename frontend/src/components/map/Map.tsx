6b72351f84a889862618d76740f5457a933f0282
uuganbayar <d.uuganbayar.stu@gmail.com>
2022-09-06 18:18:15 +0800

    mapyt

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
-import Map, { Popup } from 'react-map-gl';
-import "mapbox-gl/dist/mapbox-gl.css";
+import React from 'react';
+// import Map, { Popup } from 'react-map-gl';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
+// import "mapbox-gl/dist/mapbox-gl.css";
 import './map.css'
-import PopupHotel from './PopupHotel';
-import classNames from 'classnames';
+// import PopupHotel from './PopupHotel';
+// import classNames from 'classnames';
 import { Hotel } from '../../typings';
-import NumberFormat from 'react-number-format';
+// import NumberFormat from 'react-number-format';
 
 
 type Props = {
@@ -13,64 +14,80 @@ type Props = {
 }
 
 const Mapbox = ({ hotels }: Props) => {
-  const [viewState, setViewState] = React.useState({
-    latitude: 47.9225,
-    longitude: 107.009,
-  });
-  const [clickedHotel, setClickedHotel] = useState<string | null>(null)
+  // const [viewState, setViewState] = React.useState({
+  //   latitude: 47.9225,
+  //   longitude: 107.009,
+  // });
+  // const [clickedHotel, setClickedHotel] = useState<string | null>(null)
 
-  const handleClick = (lat: number, long: number, hotelId: string) => {
-    // setViewState({
-    //   latitude: lat +0.1,
-    //   longitude: long,
-    // })
-    setClickedHotel(hotelId)
-  }
+  // const handleClick = (lat: number, long: number, hotelId: string) => {
+  //   // setViewState({
+  //   //   latitude: lat +0.1,
+  //   //   longitude: long,
+  //   // })
+  //   setClickedHotel(hotelId)
+  // }
+
+  const Map = ReactMapboxGl({
+    accessToken: process.env.REACT_APP_MAP_TOKEN as string
+  });
+
 
   return (
+    // <Map
+    //   {...viewState}
+    //   initialViewState={{
+    //     zoom: 9,
+    //   }}
+    //   onMove={evt => !clickedHotel && setViewState(evt.viewState)}
+    //   mapStyle="mapbox://styles/temujin469/cl7g1z4p2001114nyga575att"
+    //   mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN as string}
+    //   style={{
+    //     transition: "all",
+    //     transitionDuration: "500s"
+    //   }}
+    //   // onMouseOut={() => setClickedHotel(null)}
+    //   onDragStart={() => setClickedHotel(null)}
+    // >
+    //   {
+    //     hotels?.map(hotel => (
+    //       <div key={hotel.id}>
+    //         <Popup
+    //           longitude={hotel.lacation.long}
+    //           latitude={hotel.lacation.lat}
+    //           closeButton={false}
+    //           className={classNames('pin cursor-pointer p-0 hover:z-10 select-none', { "hidden": hotel.id === clickedHotel })}>
+    //           <p onClick={() => handleClick(hotel.lacation.lat, hotel.lacation.long, hotel.id)}>
+    //             <NumberFormat value={hotel.price} displayType={'text'}
+    //               thousandSeparator={true} />₮
+    //           </p>
+    //         </Popup>
+    //         {hotel.id === clickedHotel && (
+    //           <Popup
+    //             latitude={hotel.lacation.lat}
+    //             longitude={hotel.lacation.long}
+    //             closeButton={true}
+    //             closeOnClick={false}
+    //             onClose={() => setClickedHotel(null)}
+    //             className='z-20'
+    //           >
+    //             <PopupHotel hotel={hotel} />
+    //           </Popup>)
+    //         }
+    //       </div>
+    //     ))
+    //   }
+    // </Map>
     <Map
-      {...viewState}
-      initialViewState={{
-        zoom: 9,
+      style="mapbox://styles/mapbox/streets-v9"
+      containerStyle={{
+        height: '100%',
+        width: '100%'
       }}
-      onMove={evt => !clickedHotel && setViewState(evt.viewState)}
-      mapStyle="mapbox://styles/temujin469/cl7g1z4p2001114nyga575att"
-      mapboxAccessToken={process.env.REACT_APP_MAP_TOKEN as string}
-      style={{
-        transition: "all",
-        transitionDuration: "500s"
-      }}
-      // onMouseOut={() => setClickedHotel(null)}
-      onDragStart={() => setClickedHotel(null)}
     >
-      {
-        hotels?.map(hotel => (
-          <div key={hotel.id}>
-            <Popup
-              longitude={hotel.lacation.long}
-              latitude={hotel.lacation.lat}
-              closeButton={false}
-              className={classNames('pin cursor-pointer p-0 hover:z-10 select-none', { "hidden": hotel.id === clickedHotel })}>
-              <p onClick={() => handleClick(hotel.lacation.lat, hotel.lacation.long, hotel.id)}>
-                <NumberFormat value={hotel.price} displayType={'text'}
-                  thousandSeparator={true} />₮
-              </p>
-            </Popup>
-            {hotel.id === clickedHotel && (
-              <Popup
-                latitude={hotel.lacation.lat}
-                longitude={hotel.lacation.long}
-                closeButton={true}
-                closeOnClick={false}
-                onClose={() => setClickedHotel(null)}
-                className='z-20'
-              >
-                <PopupHotel hotel={hotel} />
-              </Popup>)
-            }
-          </div>
-        ))
-      }
+      <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
+        <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
+      </Layer>
     </Map>
   );
 };
